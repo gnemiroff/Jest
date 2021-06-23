554d44b02efc62d9e65acba680dbe5e664d982dc
gnemiroff <53767867+gnemiroff@users.noreply.github.com>
2021-06-23 02:26:21 +0200

    Update functions.test.js
    2+2

--- functions.test.js
+++ functions.test.js
@@ -21,10 +21,10 @@ test('null', () => {
 
 // Number tests
 test('two plus two', () => {
-    const value = 2 + 3;
+    const value = 2 + 2;
     expect(value).toBeGreaterThan(3);
     expect(value).toBeGreaterThanOrEqual(3.5);
-    //expect(value).toBeLessThan(5);
+    expect(value).toBeLessThan(5);
     expect(value).toBeLessThanOrEqual(4.5);
   
     // toBe and toEqual are equivalent for numbers
